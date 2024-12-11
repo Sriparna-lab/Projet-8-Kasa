3fcbf5e6ca346341491c1e2de915a564a6b7273e
Sri <name@example.com>
2024-12-11 11:48:58 +0100

    Addition od collapse in about page

--- src/Pages/about/About.js
+++ src/Pages/about/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 import Header from "../../Components/header/Header";
 import Banner from "../../Components/Banner/Banner";
 import Footer from "../../Components/Footer/Footer";
+import Collapse from "../../Components/Collapse/Collapse";
 import BannerAbout from "../../Assets/Banner/BannerAbout.png";
+import CollapseData from "../../Data/collapses.json";
 
 function About() {
     return (
@@ -10,8 +12,19 @@ function About() {
             <Header />
             <main>
                 <Banner 
-                image={BannerAbout} 
-                className= "banner__about"/>
+                    image={BannerAbout} 
+                    className= "banner__about"/>
+                <div className="collapse">
+                    <div className="collapse__dropdown">
+                        {CollapseData.map((item) => {
+                    return (
+                        <div key={item.id}>
+                        <Collapse content={item.content} title={item.title} />
+                    </div>
+                );
+            })}
+            </div>
+        </div>
             </main>
             <Footer />
         </div>
